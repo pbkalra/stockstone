d88dee566d03040b68489ba15279882754c64f5f
pkalra-hgse <prk597@mail.harvard.edu>
2020-06-29 07:00:06 +0000

    lets try to get the actual graph

---
+++ stockgraph.py
@@ -0,0 +1,34 @@
+def stockgraph(StockSymb)
+
+    csvurl = ('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + StockSymb + '&apikey=ECO7VXU7CT9JLWCT&datatype=csv')
+
+    csvstockdata = requests.get(csvurl)
+
+    with open('stockdata.csv', 'wb') as f:
+           f.write(csvstockdata.content)
+       
+    csvstockdf = pd.read_csv('stockdata.csv')
+
+    jsonstockdf = pd.read_json(jsonstockdata.content)
+
+    #print(csvstockdf.columns)
+
+    from bokeh.layouts import gridplot
+    from bokeh.plotting import figure, output_file, show
+
+    def datetime(x):
+        return np.array(x, dtype=np.datetime64)
+
+    #filter for one month--either set or ask user to choose month
+
+    p1 = figure(x_axis_type="datetime", title="Stock Closing Prices")
+    p1.xaxis.axis_label = 'Date'
+    p1.yaxis.axis_label = 'Price'
+    p1.line(datetime(csvstockdf['timestamp']), csvstockdf['close'], color='#A6CEE3', legend_label=StockSymb)
+    #p1.line(datetime(csvstockdf['timestamp']), csvstockdf['open'], color='#B2DF8A', legend_label='open')
+
+    outfilename = 'template/time_series.html' #+ StockSymb 
+
+    output_file(outfilename, title="adapted from stocks.py example")
+
+    return show(p1)
