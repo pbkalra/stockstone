b902f4fc7ea7d3221597950835b091f312d45cc8
pkalra-hgse <prk597@mail.harvard.edu>
2020-06-29 05:02:56 +0000

    copied app.py from flask-framework instead of lulu

--- app.py
+++ app.py
@@ -1,32 +1,14 @@
-from flask import Flask,render_template,request,redirect
-app_lulu = Flask(__name__)
+from flask import Flask, render_template, request, redirect
 
-app_lulu.vars={}
+app = Flask(__name__)
 
-@app_lulu.route('/index_lulu',methods=['GET','POST'])
-def index_lulu():
-    nquestions=5
-    if request.method == 'GET':
-        return render_template('userinfo_lulu.html',num=nquestions)
-    else:
-        #request was a POST
-        app_lulu.vars['name'] = request.form['name_lulu']
-        app_lulu.vars['age'] = request.form['age_lulu']
+@app.route('/')
+def index():
+  return render_template('index.html')
 
-        f = open('%s_%s.txt'%(app_lulu.vars['name'],app_lulu.vars['age']),'w')
-        f.write('Name: %s\n'%(app_lulu.vars['name']))
-        f.write('Age: %s\n\n'%(app_lulu.vars['age']))
-        f.close()
+@app.route('/about')
+def about():
+  return render_template('about.html')
 
-        return render_template('layout_lulu.html',num=1,question='How many eyes do you have?',ans1='1',ans2='2',ans3='3')
-
-@app_lulu.route('/next_lulu',methods=['POST'])
-def next_lulu():
-    return redirect('/usefulfunction_lulu')
-
-@app_lulu.route('/usefulfunction_lulu',methods=['GET','POST'])
-def usefulfunction_lulu():
-    return render_template('layout_lulu.html',num=1,question='Which fruit do you like best?',ans1='banana',ans2='mango',ans3='pineapple')
-
-if __name__ == "__main__":
-    app_lulu.run()
+if __name__ == '__main__':
+  app.run(port=33507)
